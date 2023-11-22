a81a2153dab59b4d4c35d9d46386aded451b99af
Jói Sigurdsson <joi@crankwheel.com>
2023-11-22 20:42:38 +0000

    First iteration of Turtles - the Shelly management app

---
+++ config/config.exs
@@ -0,0 +1,4 @@
+import Config
+
+config :turtles,
+  ip_base: "192.168.1."
---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Turtles.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :turtles,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {Turtles, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:httpoison, "~> 2.0"},
+      {:jason, "~> 1.4"}
+    ]
+  end
+end
